6b58f73b898a5e84e8cca729e09d0d32c28b9e65
1n5aN1aC <joshua@mailmag.net>
2016-11-17 10:07:47 -0800

    Move functionality out to seperate functions

--- NTPclock/NTPclock.ino
+++ NTPclock/NTPclock.ino
@@ -24,40 +24,22 @@ char pass[] = "";       // your network password           //
 //---------------------------------------------------------//
 
 LedControl lc1 = LedControl(D5,D6,D7,1); //Initialize MAX7219
-char timeDigits[] = "----00";           //Most recent 
+char timeDigits[] = "----";              //String represntation of current time
 
-void setup()
-{
+// Initial set up routines
+void setup() {
   Serial.begin(115200);
   Serial.println();
-  Serial.println();
 
-  // We start by connecting to a WiFi network
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("  Done.");
-  
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  
-  Serial.print("Starting NTP client...  ");
-  NTP.begin("pool.ntp.org", 4, true);
-  NTP.setInterval(60);
-  delay(2000);
-  Serial.println("Done.");
+  connectWifi(); //First, connect to wifi
+  beginNTP();    //Start up NTP Client & time keeping
 
   lc1.shutdown(0,false);
   lc1.setIntensity(0, 10);
 }
 
-//
-void loop()
-{
+// Main program loop
+void loop() {
   String timeDisplay = NTP.getTimeStr();
   char minDig1 = timeDisplay.charAt(3);
   char minDig2 = timeDisplay.charAt(4);
@@ -68,18 +50,46 @@ void loop()
   lc1.setChar(0, 2, secDig1, false);
   lc1.setChar(0, 3, secDig2, false);
   
-  lc1.setChar(0, 4, secDig2, false);
+  lc1.setChar(0, 4, secDig2, false); //test for progress bar
   
   Serial.println(timeDisplay);
   
-  time_t t = now(); // store the current time in time variable t
-  hour(t);          // returns the hour for the given time t
-  minute(t);        // returns the minute for the given time t
-  second(t);        // returns the second for the given time t
-  day(t);           // the day for the given time t
-  weekday(t);       // day of the week for the given time t
-  month(t);         // the month for the given time t
-  year(t);          // the year for the given time t
+  time_t time = now(); // store the current time in time variable t
+  hour(t);             // returns the hour for the given time t
+  minute(t);           // returns the minute for the given time t
+  second(t);           // returns the second for the given time t
+  day(t);              // the day for the given time t
+  weekday(t);          // day of the week for the given time t
+  month(t);            // the month for the given time t
+  year(t);             // the year for the given time t
   
-  delay(1000);  //   REMOVE DELAY FOR A COUNTER.................................................../////////////////////////
+  delay(1000);         //   REMOVE DELAY FOR A COUNTER..............................PLEASE!!!!......../////////////////////////
+}
+
+// Inition connection to WiFi
+void connectWifi() {
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("  Done.");
+  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// Initial set up of NTP
+void beginNTP() {
+  Serial.print("Starting NTP client.");
+  NTP.begin("pool.ntp.org", 4, true);
+  NTP.setInterval(60);
+  while (NTP.getLastNTPSync() == 0) {     //returns a time_t, so not sure this will work or not.
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Done.");
 }
