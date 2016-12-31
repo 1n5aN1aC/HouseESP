bf6f1700c14afcd01a8963b0baaba72a8042b984
1n5aN1aC <joshuavillwo@gmail.com>
2016-12-31 09:31:53 -0800

    Add options file for NTPclock

--- NTPclock/NTPclock.ino
+++ NTPclock/NTPclock.ino
@@ -1,10 +1,10 @@
 //----------------------------------------------------------------------------------------------------------------
 // Clock.ino
-// 
+//
 // Controls a LED clock using an ESP8266, several 7-segment displays, and a bargraph display
 // Uses NTP for internet time updating, and supports a Real-Time clock for outages.
 // Some extra features include a temperature & humidity sensor, brightness control, time zone control.
-// 
+//
 // Author - Joshua Villwock
 // Created - 2016-11-13
 // License - Mozilla Public License 2.0 (Do what you want, credit the author, must release under same license)
@@ -15,13 +15,10 @@
 #include "LEDHelper.h"
 #include "TimeManager.h"
 #include "MQTTHelper.h"
+#include "Options.cpp"
 
-//---------------------------------------------------------//
-//              CONFIGURE YOUR NETWORK HERE                //
-//---------------------------------------------------------//
-#define SSID "joshua"  // your network SSID (name)         //
-#define PASS ""        // your network password            //
-//---------------------------------------------------------//
+extern const char WIFI_SSID[];
+extern const char WIFI_PASS[];
 
 unsigned long displayLastUpdated = millis();
 unsigned long lastTempSend = millis();
@@ -45,9 +42,9 @@ void setup() {
 // Main program loop
 void loop() {
   MQTT_Helper.mqttLoop();
-  
-  checkUpdate();
-  checkTemp();
+
+  checkTimeUpdate();
+  checkTempUpdate();
   yield();
 }
 
@@ -55,28 +52,28 @@ void loop() {
 // We wait for 5 seconds to connect, but do not block on the connection.
 void connectWifi() {
   Serial.print("Connecting to ");
-  Serial.println(SSID);
-  WiFi.begin(SSID, PASS);
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   delay(5000);
 }
 
 //Update the clock if need be
-void checkUpdate() {
+void checkTimeUpdate() {
   if (millis() > (displayLastUpdated + DISPLAY_UPDATE_FREQUENCY)) {
     LED_Helper.updateDigits(); //Update the time display
     LED_Helper.updateMisc();   //Update the rest of the display
-    
+
     Serial.println(NTP.getTimeDateString() );
-    
+
     displayLastUpdated = millis();
   }
 }
 
 //Send temp update if need be
-void checkTemp() {
+void checkTempUpdate() {
   if (millis() > lastTempSend + TEMPERATURE_UPDATE_FREQUENCY) {
     float temp = Time_Manager.getTemperature();
-  
+
     char result[8]; // Buffer big enough for 7-character float
     dtostrf(temp, 6, 2, result); // Leave room for too large numbers!
     MQTT_Helper.publishMQTT("home/jroom/clock/temp", result, false);
---
+++ NTPclock/Options.cpp
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------------------------------------------------------
+// Options.cpp
+//
+// This file exists to contain potentially-sesitive configuration options,
+// such as network passwords and the like.
+// It does, however, contain other options as well.
+//
+// Author - Joshua Villwock
+// Created - 2016-12-30
+// License - Mozilla Public License 2.0 (Do what you want, credit the author, must release under same license)
+//----------------------------------------------------------------------------------------------------------------
+
+// WiFi settings
+const char WIFI_SSID[] = "joshua";  // your network SSID (name)
+const char WIFI_PASS[] = "";        // your network password
+
+// OTA settings
+const char OTA_PASS[] = "21232f297a57a5a743894a0e4a801fc3";
