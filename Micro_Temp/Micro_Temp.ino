5ce7d656a848c33b633361d8102bd48112e0a387
1n5aN1aC <joshua@mailmag.net>
2018-03-11 14:42:10 -0700

    Enable deep sleep

--- Micro_Temp/Micro_Temp.ino
+++ Micro_Temp/Micro_Temp.ino
@@ -12,63 +12,80 @@
 
 #include <Arduino.h>
 #include <DHT.h>
-#include "MQTTHelper.h"
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include "Options.cpp"
+#include "MQTTHelper.h"
 extern "C" {
   #include "user_interface.h"
 }
 
-extern const char WIFI_SSID[];
-extern const char WIFI_PASS[];
+void connectWifi();
+void checkTempHumid();
 
-#define DHTTYPE DHT22     // DHT 22  (AM2302), AM2320, AM2321
-const int DHTPin = 2;
-bool fahrenheit = true;
+#define UPDATE_FREQUENCY 20 // Time to sleep (in seconds)
+#define DHTTYPE DHT11       // DHT 11
+const int DHTPin = 2;       // Should be D4 on the Wemos D1 Mini
+bool fahrenheit = true;     // Yes, report fahrenheit
 DHT dht(DHTPin, DHTTYPE);
 
-unsigned long lastTempHumidSend = millis();
-#define TEMP_HUMID_UPDATE_FREQUENCY 60000
+//Wifi settings
+extern const char WIFI_SSID[];
+extern const char WIFI_PASS[];
+WiFiClientSecure wifiClient;
 
-// Initial set up routines
+//Main progrom upon return from Deep Sleep
 void setup() {
+  Serial.begin(9600);
+  while(!Serial) { }         // Wait for serial to initialize.
+  Serial.println("I AM WOKE.");
+  
   dht.begin();
-  WiFi.mode(WIFI_STA);
-  wifi_station_set_hostname("ESP_Attic");
   connectWifi();
   MQTT_Helper.setup();
+  yield();
+  checkTempHumid();
 }
 
-// Main program loop
 void loop() {
-  MQTT_Helper.mqttLoop();
-  checkTempHumid();
-  yield();
-  delay(100); //saves considerable power & heat
+  delay(2000);  //Sometimes the MQTT doesn't seem to go through before sleep begins....
+  Serial.println("Going into deep sleep for 20 seconds");
+  ESP.deepSleep(UPDATE_FREQUENCY * 1000000);
 }
 
 // Initial connection to WiFi
 // We wait for 5 seconds to connect, but do not block on the connection.
 void connectWifi() {
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  delay(5000);
-}
+  // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_OFF);
+  WiFi.mode(WIFI_STA);
+  wifi_station_set_hostname("ESP_Attic");
+  unsigned long wifiConnectStart = millis();
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    if (WiFi.status() == WL_CONNECT_FAILED) {
+      Serial.println("Failed to connect to WiFi. Please verify credentials: ");
+      delay(10000);
+    }
 
-// Send temp / humidity update if need be
-void checkTempHumid() {
-  if (millis() > lastTempHumidSend + TEMP_HUMID_UPDATE_FREQUENCY) {
-
-    char temp[8]; // Buffer big enough for 7-character float
-    dtostrf(getTemperature(), -6, 2, temp); // Leave room for too large numbers!
-
-    char humid[8];
-    dtostrf(getHumidity(), -6, 2, humid);
-
-    MQTT_Helper.publishMQTT("home/living/micro/temp",  temp,  false);
-    MQTT_Helper.publishMQTT("home/living/micro/humid", humid, false);
-    lastTempHumidSend = millis();
+    delay(500);
+    Serial.print("Connecting wifi");
+    Serial.print(".");
+    // Only try for 5 seconds.
+    if (millis() - wifiConnectStart > 15000) {
+      Serial.println("Failed to connect to WiFi");
+      return;
+    }
   }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
+
 
 float getTemperature() {
   return dht.readTemperature(fahrenheit);
@@ -76,4 +93,16 @@ float getTemperature() {
 
 float getHumidity() {
   return dht.readHumidity();
+}
+
+// Send temp / humidity update if need be
+void checkTempHumid() {
+  char temp[8]; // Buffer big enough for 7-character float
+  dtostrf(getTemperature(), -6, 2, temp); // Leave room for too large numbers!
+
+  char humid[8];
+  dtostrf(getHumidity(), -6, 2, humid);
+
+  MQTT_Helper.publishMQTT("home/living/micro/temp",  temp,  false);
+  MQTT_Helper.publishMQTT("home/living/micro/humid", humid, false);
 }
